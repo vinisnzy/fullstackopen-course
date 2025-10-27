7f1469e138bc6c50495ca7e4e06782467c31469a
Vinícius de Paula Moraes <manoseilacara123@gmail.com>
2025-10-27 19:10:56 -0300

    feat: exercise 1.4

--- part1/src/App.js
+++ part1/src/App.js
@@ -19,41 +19,47 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part part={props.part1.part} exercise={props.part1.exercises} />
-      <Part part={props.part2.part} exercise={props.part2.exercises} />
-      <Part part={props.part3.part} exercise={props.part3.exercises} />
+      <Part part={props.parts[0].part} exercise={props.parts[0].exercises} />
+      <Part part={props.parts[1].part} exercise={props.parts[1].exercises} />
+      <Part part={props.parts[2].part} exercise={props.parts[2].exercises} />
     </div>
   );
 };
 
 const Total = (props) => {
+  let sum = 0;
+  props.parts.forEach((element) => {
+    sum += element.exercises;
+  });
   return (
     <div>
-      <p>Number of exercises {props.total}</p>
+      <p>Number of exercises {sum}</p>
     </div>
   );
 };
 
 const App = () => {
   const course = "Desenvolvimento de aplicação Half Stack";
-  const part1 = {
-    part: "Fundamentos da biblioteca React",
-    exercises: 10,
-  };
-  const part2 = {
-    part: "Usando props para passar dados",
-    exercises: 7,
-  };
-  const part3 = {
-    part: "Estado de um componente",
-    exercises: 14,
-  };
+  const parts = [
+    {
+      part: "Fundamentos da biblioteca React",
+      exercises: 10,
+    },
+    {
+      part: "Usando props para passar dados",
+      exercises: 7,
+    },
+    {
+      part: "Estado de um componente",
+      exercises: 14,
+    },
+  ];
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3}/>
-      <Total total={part1.exercises + part2.exercises + part3.exercises} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   );
 };
