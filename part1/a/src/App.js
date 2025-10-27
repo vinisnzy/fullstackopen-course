5f5980f7d17ce9f32806248e3ffa7c2b3a20cf3a
Vinícius de Paula Moraes <manoseilacara123@gmail.com>
2025-10-27 18:44:55 -0300

    Revert "feat: exercise 1.2"
    This reverts commit 84e6e5b4967e4099235cd18bd0f759cc6becc3ba.

--- part1/a/src/App.js
+++ part1/a/src/App.js
@@ -1,7 +1,3 @@
-const exercises1 = 10;
-const exercises2 = 7;
-const exercises3 = 14;
-
 const Header = (props) => {
   return (
     <div>
@@ -10,25 +6,12 @@ const Header = (props) => {
   );
 };
 
-const Part = (props) => {
+const Content = (props) => {
   return (
     <div>
       <p>
         {props.part} {props.exercise}
       </p>
-    </div>
-  )
-}
-
-const Content = () => {
-  const part1 = "Fundamentos da biblioteca React";
-  const part2 = "Usando props para passar dados";
-  const part3 = "Estado de um componente";
-  return (
-    <div>
-      <Part part={part1} exercise={exercises1} />
-      <Part part={part2} exercise={exercises2} />
-      <Part part={part3} exercise={exercises3} />
     </div>
   );
 };
@@ -36,19 +19,27 @@ const Content = () => {
 const Total = (props) => {
   return (
     <div>
-      <p>Number of exercises {props.total}</p>
+      <p>Number of exercises {props.totalExercise}</p>
     </div>
   );
 };
 
 const App = () => {
   const course = "Desenvolvimento de aplicação Half Stack";
+  const part1 = "Fundamentos da biblioteca React";
+  const exercises1 = 10;
+  const part2 = "Usando props para passar dados";
+  const exercises2 = 7;
+  const part3 = "Estado de um componente";
+  const exercises3 = 14;
 
   return (
     <div>
       <Header course={course} />
-      <Content />
-      <Total total={exercises1 + exercises2 + exercises3} />
+      <Content part={part1} exercise={exercises1} />
+      <Content part={part2} exercise={exercises2} />
+      <Content part={part3} exercise={exercises3} />
+      <Total totalExercise={exercises1 + exercises2 + exercises3} />
     </div>
   );
 };
