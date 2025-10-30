e05b724f6a8551e2c46adfcb1ba796ee94de16c9
Vinícius de Paula Moraes <manoseilacara123@gmail.com>
2025-10-29 22:59:40 -0300

    feat: exercise 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -8,32 +8,39 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
 
   const handleGood = () => {
     const newValue = good + 1
     setGood(newValue)
+    setTotal(total + 1)
   }
 
   const handleNeutral = () => {
     const newValue = neutral + 1
     setNeutral(newValue)
+    setTotal(total + 1)
   }
 
   const handleBad = () => {
     const newValue = bad + 1
     setBad(newValue)
+    setTotal(total + 1)
   }
 
   return (
     <div>
-      <h1>Give feedback</h1>
+      <h1>Give Feedback</h1>
       <Button handleEvent={() => handleGood()} text="Good" />
       <Button handleEvent={() => handleNeutral()} text="Neutral" />
       <Button handleEvent={() => handleBad()} text="Bad" />
       <div>
-        <p>Good {good}</p>
-        <p>Neutral {neutral}</p>
-        <p>Bad {bad}</p>
+        <p>Good: {good}</p>
+        <p>Neutral: {neutral}</p>
+        <p>Bad: {bad}</p>
+        <p>Total: {total}</p>
+        <p>Average: {(good * 1 + neutral * 0 + bad * -1) / total}</p>
+        <p>Positive:{(good / total) * 100}%</p>
       </div>
     </div>
   )
